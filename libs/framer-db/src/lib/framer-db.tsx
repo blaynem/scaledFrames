b3ffe0bd1a072d4935f1fa9789ad28ac1c17127a
Blayne Marjama <blayne.marjama@gmail.com>
2024-04-28 19:59:17 -0700

    moved to the correct dir

--- libs/framer-db/src/lib/framer-db.tsx
+++ libs/framer-db/src/lib/framer-db.tsx
@@ -1,9 +1,11 @@
+import styles from './framer-db.module.css';
+
 /* eslint-disable-next-line */
 export interface FramerDbProps {}
 
 export function FramerDb(props: FramerDbProps) {
   return (
-    <div>
+    <div className={styles['container']}>
       <h1>Welcome to FramerDb!</h1>
     </div>
   );
