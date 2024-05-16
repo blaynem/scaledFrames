e0addf05765ac8c7b8123f013107cd51fc32204a
Blayne Marjama <blayne.marjama@gmail.com>
2024-05-16 11:45:20 -0700

    use hono jwt

--- libs/FramerServerSDK/src/lib/server/supabaseServer.ts
+++ libs/FramerServerSDK/src/lib/server/supabaseServer.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import jwt from 'jsonwebtoken';
 import { type NextRequest, type NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import { deleteCookie, getCookie, setCookie } from 'cookies-next';
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { verify } from 'hono/jwt';
 
 // server component can only get cookies and not set them, hence the "component" check
 export function createSupabaseServerClient(component = false) {
@@ -71,7 +71,7 @@ export const decodeJwt = async (
 ): Promise<{ email: string; id: string }> => {
   const token = access_token.split(' ')[1];
   try {
-    const decoded = jwt.verify(token, process.env['SUPABASE_JWT_SECRET']!);
+    const decoded = await verify(token, process.env['SUPABASE_JWT_SECRET']!);
     if (!decoded.email) {
       throw new Error('Could not get email from JWT.');
     }
